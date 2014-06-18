2f9c75a814b35e45cec0ed08989e11529d417d71
Adrian Sinclair <adrian@adrusi.com>
2014-06-18 14:17:07 -0400

    added register token and support for decimal and hexadecimal numerals

--- src/test_tok.c
+++ src/test_tok.c
@@ -36,8 +36,11 @@ test (tokread_sym) {
   f = file_with_contents("");
   assert(!tokread_sym(f, &t));
   f = file_with_contents("c");
-  assert(tokread_sym(f, &t));
+  tokread_sym(f, &t);
   assert(fgetc(f) == EOF);
+  f = file_with_contents("abc def");
+  tokread_sym(f, &t);
+  assert(fgetc(f) == ' ');
 }
 
 test (tokread_ins) {
@@ -51,12 +54,162 @@ test (tokread_ins) {
   f = file_with_contents("add");
   assert(tokread_ins(f, &t));
   assert(t.data.ins == 1);
+  f = file_with_contents("foo");
+  assert(!tokread_ins(f, &t));
+  f = file_with_contents("ld");
+  tokread_ins(f, &t);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("ld foo");
+  tokread_ins(f, &t);
+  assert(fgetc(f) == ' ');
+}
+
+test (tokread_reg) {
+  tok t;
+  FILE *f;
+  tok_registers[0] = "HL";
+  tok_registers[1] = "A";
+  f = file_with_contents("HL");
+  assert(tokread_reg(f, &t));
+  assert(t.data.reg == 0);
+  f = file_with_contents("A");
+  assert(tokread_reg(f, &t));
+  assert(t.data.reg == 1);
+  f = file_with_contents("foo");
+  assert(!tokread_reg(f, &t));
+  f = file_with_contents("HL");
+  tokread_reg(f, &t);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("HL foo");
+  tokread_reg(f, &t);
+  assert(fgetc(f) == ' ');
+}
+
+test (read_dec) {
+  tok t;
+  FILE *f;
+  f = file_with_contents("123");
+  assert(read_dec(f, &t));
+  assert(t.data.num == 123);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("123 abc");
+  assert(read_dec(f, &t));
+  assert(fgetc(f) == ' ');
+}
+
+test (read_hex) {
+  tok t;
+  FILE *f;
+  f = file_with_contents("0xABC123");
+  assert(read_hex(f, &t));
+  assert(t.data.num == 0xABC123);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("0xABC123 abc");
+  assert(read_hex(f, &t));
+  assert(fgetc(f) == ' ');
+  f = file_with_contents("$ABC123");
+  assert(read_hex(f, &t));
+  assert(t.data.num == 0xABC123);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("$ABC123 abc");
+  assert(read_hex(f, &t));
+  assert(fgetc(f) == ' ');
+  f = file_with_contents("ABC123h");
+  assert(read_hex(f, &t));
+  assert(t.data.num == 0xABC123);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("ABC123h abc");
+  assert(read_hex(f, &t));
+  assert(fgetc(f) == ' ');
+}
+
+test (read_oct) {
+  tok t;
+  FILE *f;
+  f = file_with_contents("0123");
+  assert(read_oct(f, &t));
+  assert(t.data.num == 0123);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("0123 abc");
+  assert(read_oct(f, &t));
+  assert(fgetc(f) == ' ');
+  f = file_with_contents("123o");
+  assert(read_oct(f, &t));
+  assert(t.data.num == 0123);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("123o abc");
+  assert(read_oct(f, &t));
+  assert(fgetc(f) == ' ');
+}
+
+test (read_bin) {
+  tok t;
+  FILE *f;
+  f = file_with_contents("0b111100000001");
+  assert(read_bin(f, &t));
+  assert(t.data.num == 0xF01);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("0b111100000001 abc");
+  assert(read_hex(f, &t));
+  assert(fgetc(f) == ' ');
+  f = file_with_contents("%111100000001");
+  assert(read_bin(f, &t));
+  assert(t.data.num == 0xF01);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("%111100000001 abc");
+  assert(read_hex(f, &t));
+  assert(fgetc(f) == ' ');
+  f = file_with_contents("111100000001b");
+  assert(read_bin(f, &t));
+  assert(t.data.num == 0xF01);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("111100000001b abc");
+  assert(read_hex(f, &t));
+  assert(fgetc(f) == ' ');
+}
+
+test (read_char) {
+  tok t;
+  FILE *f;
+  f = file_with_contents("'a'");
+  assert(read_char(f, &t));
+  assert(t.data.num == 'a');
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("'a' abc");
+  assert(read_char(f, &t));
+  assert(fgetc(f) == ' ');
+  f = file_with_contents("'\\\\'");
+  assert(read_char(f, &t));
+  assert(t.data.num == '\\');
+  f = file_with_contents("'\\''");
+  assert(read_char(f, &t));
+  assert(t.data.num == '\'');
+}
+
+test (read_bool) {
+  tok t;
+  FILE *f;
+  f = file_with_contents("true");
+  assert(read_bool(f, &t));
+  assert(t.data.num == 1);
+  assert(fgetc(f) == EOF);
+  f = file_with_contents("false abc");
+  assert(read_bool(f, &t));
+  assert(t.data.num == 0);
+  assert(fgetc(f) == ' ');
 }
 
 suite {
   test_case(tokread_eof);
   test_case(tokread_sym);
   test_case(tokread_ins);
+  test_case(tokread_reg);
+  test_case(read_dec);
+  test_case(read_hex);
+  test_case(read_oct);
+  test_case(read_bin);
+  test_case(read_char);
+  test_case(read_bool);
   return 0;
 }
   
--- src/tok.c
+++ src/tok.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "strbuf.h"
 #include "strutil.h"
+#include "dbg.h"
 
 char *tok_mnemonics[64];
 char *tok_registers[64];
@@ -53,5 +54,140 @@ int tokread_ins(FILE *f, tok *t) {
     return 1;
   }
   strbuf_free(s);
+  return 0;
+}
+
+int tokread_reg(FILE *f, tok *t) {
+  t->type = TOK_REG;
+  strbuf s = strbuf_new();
+  char c;
+  while (alphabetic(c = fgetc(f))) strbuf_appendchar(&s, c);
+  if (c != EOF) fseek(f, -1, SEEK_CUR);
+  int i;
+  for (i = 0; tok_registers[i]; i++)
+  if (strcmp(tok_registers[i], s.string) == 0) {
+    t->data.reg = i;
+    strbuf_free(s);
+    return 1;
+  }
+  strbuf_free(s);
+  return 0;
+}
+
+static int read_dec(FILE *f, tok *t);
+static int read_hex(FILE *f, tok *t);
+static int read_oct(FILE *f, tok *t);
+static int read_bin(FILE *f, tok *t);
+static int read_char(FILE *f, tok *t);
+static int read_bool(FILE *f, tok *t);
+
+#define NUM_NUM_READERS 6
+int tokread_num(FILE *f, tok *t) {
+  t->type = TOK_NUM;
+  static tok_reader readers[NUM_NUM_READERS] = {
+    read_hex, read_oct, read_bin, read_dec, read_char, read_bool
+  };
+  long mark = ftell(f);
+  int i;
+  for (i = 0; i < NUM_NUM_READERS; i++) {
+    if (readers[i](f, t)) return 1;
+    else fseek(f, mark, SEEK_SET);
+  }
+  return 0;
+}
+
+static int read_dec(FILE *f, tok *t) {
+  t->data.num = 0;
+  char c = fgetc(f);
+  if (!numeric(c)) return 0;
+  do {
+    t->data.num *= 10;
+    t->data.num += c - '0';
+  } while (numeric(c = fgetc(f)));
+  if (c != EOF) fseek(f, -1, SEEK_CUR);
+  return 1;
+}
+
+static int read_hex_c(FILE *f, tok *t);
+static int read_hex_asm(FILE *f, tok *t);
+static int read_hex_post(FILE *f, tok *t);
+
+#define NUM_HEX_READERS 3
+static int read_hex(FILE *f, tok *t) {
+  static tok_reader readers[NUM_HEX_READERS] = {
+    read_hex_c, read_hex_asm, read_hex_post
+  };
+  long mark = ftell(f);
+  int i;
+  for (i = 0; i < NUM_HEX_READERS; i++) {
+    if (readers[i](f, t)) return 1;
+    else fseek(f, mark, SEEK_SET);
+  }
+  return 0;
+}
+
+static int is_hex_digit(char c) {
+  return numeric(c) || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
+}
+
+static int read_hex_c(FILE *f, tok *t) {
+  t->data.num = 0;
+  char c;
+  c = fgetc(f);
+  if (c != '0') return 0;
+  c = fgetc(f);
+  if (c != 'x') return 0;
+  while (is_hex_digit(c = fgetc(f))) {
+    t->data.num *= 0x10;
+    if (c >= '0' && c <= '9') t->data.num += c - '0';
+    else if (c >= 'A' && c <= 'F') t->data.num += c - 'A' + 0xA;
+    else t->data.num += c - 'a' + 0xa;
+  }
+  if (c != EOF) fseek(f, -1, SEEK_CUR);
+  return 1;
+}
+
+static int read_hex_asm(FILE *f, tok *t) {
+  t->data.num = 0;
+  char c;
+  c = fgetc(f);
+  if (c != '$') return 0;
+  while (is_hex_digit(c = fgetc(f))) {
+    t->data.num *= 0x10;
+    if      (c >= '0' && c <= '9') t->data.num += c - '0';
+    else if (c >= 'A' && c <= 'F') t->data.num += c - 'A' + 0xA;
+    else                           t->data.num += c - 'a' + 0xa;
+  }
+  if (c != EOF) fseek(f, -1, SEEK_CUR);
+  return 1;
+}
+
+static int read_hex_post(FILE *f, tok *t) {
+  t->data.num = 0;
+  long mark = ftell(f);
+  char c = fgetc(f);
+  if (!is_hex_digit(c)) return 0;
+  do {
+    t->data.num *= 0x10;
+    if      (c >= '0' && c <= '9') t->data.num += c - '0';
+    else if (c >= 'A' && c <= 'F') t->data.num += c - 'A' + 0xA;
+    else                           t->data.num += c - 'a' + 0xa;
+  } while (is_hex_digit(c = fgetc(f)));
+  return c == 'h';
+}
+
+static int read_oct(FILE *f, tok *t) {
+  return 0;
+}
+
+static int read_bin(FILE *f, tok *t) {
+  return 0;
+}
+
+static int read_char(FILE *f, tok *t) {
+  return 0;
+}
+
+static int read_bool(FILE *f, tok *t) {
   return 0;
 }
